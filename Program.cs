c49fa29f4c59bf26c1d930a9b529dfa761605f9e
nazrinmajidova <majidovanazrin@gmail.com>
2024-05-15 13:09:26 +0400

    newupdate

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ Console.WriteLine("Hello, Lala!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Naz!");
+Console.WriteLine("Hello, Lala!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
